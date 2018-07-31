72eb46aa377160d8c54b8678aedea8232fb64615
whitneyscott <swhitney@sfasu.edu>
2018-07-30 19:17:52 -0500

    modified port

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+const PORT = process.env.PORT;
+app.listen(process.env.PORT ||3000, ()=> {
+  console.log(`app is running on port ${process.env.PORT}`);
 })
